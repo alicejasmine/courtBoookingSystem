a03a5749addc8241a9f6334b1f7431a1a271e4e5
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-04-26 14:08:46 +0200

    add date picker

--- frontend/lib/ui/tabs/home.dart
+++ frontend/lib/ui/tabs/home.dart
@@ -1,5 +1,9 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'grid_view.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,17 +13,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  DateTime selectedDate = DateTime.now();
+
+ 
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(const Duration(days: 7)),
+    );
+    if (pickedDate != null && pickedDate != selectedDate) {
+      setState(() {
+        selectedDate = pickedDate;
+        
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-        Text(
-          'Home',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Book a Court'),
+      ),
+      body: Column(
+        children: [
+          TextButton.icon(
+            onPressed: () => _selectDate(context),
+            icon: const Icon(Icons.calendar_today),
+            label: Text('Select Date: ${DateFormat('dd/MM/yy').format(selectedDate)}'),
+          ),
+              ],
+      ),
     );
   }
 }
