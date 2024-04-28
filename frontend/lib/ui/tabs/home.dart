b03bb99e3c808a1c0a321207b2f597392fcd1456
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-04-28 18:58:13 +0200

    starting with displaying time slots

--- frontend/lib/ui/tabs/home.dart
+++ frontend/lib/ui/tabs/home.dart
@@ -1,9 +1,12 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
-import 'grid_view.dart';
+import '../../bloc/booking_bloc.dart';
+import '../../models/events.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,9 +16,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
   DateTime selectedDate = DateTime.now();
 
- 
+  
+  final Map<String, List<String>> courtTimeSlots = {
+    'Court 1': ['8:00 AM - 9:00 AM', '9:00 AM - 10:00 AM'],
+    'Court 2': ['10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM'],
+  };
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? pickedDate = await showDatePicker(
@@ -27,7 +35,10 @@ class _HomeState extends State<Home> {
     if (pickedDate != null && pickedDate != selectedDate) {
       setState(() {
         selectedDate = pickedDate;
-        
+        context.read<BookingBloc>().add(
+          ClientWantsToFetchCourtAvailability(selectedDate: selectedDate, eventType: 'ClientWantsToFetchCourtAvailability'),
+        );
+
       });
     }
   }
@@ -45,7 +56,38 @@ class _HomeState extends State<Home> {
             icon: const Icon(Icons.calendar_today),
             label: Text('Select Date: ${DateFormat('dd/MM/yy').format(selectedDate)}'),
           ),
+          Expanded(
+            child: GridView.count(
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              crossAxisCount: 2,
+              children: [
+                for (var court in courtTimeSlots.keys) 
+                  Column(
+                    children: [
+                      Text(
+                        court,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      for (var timeSlot in courtTimeSlots[court]!) 
+                        Container(
+                          color: Colors.blue,
+                          alignment: Alignment.center,
+                          child: Text(
+                            timeSlot,
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                          height: 40,
+                        ),
+                    ],
+                  ),
               ],
+            ),
+          ),
+        ],
       ),
     );
   }
