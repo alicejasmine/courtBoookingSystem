12dc0d80487c255e9e7d2f066d345b72ab3e358c
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-05-10 10:27:02 +0200

    add event handler to delete a booking

---
+++ api/ClientEventHandlers/ClientWantsToDeleteBooking.cs
@@ -0,0 +1,25 @@
+﻿using api.ClientEventFilters;
+using api.Repositories;
+using Fleck;
+using lib;
+
+namespace api.ClientEventHandlers;
+
+public class ClientWantsToDeleteBookingDto : BaseDto
+{
+    public int BookingId { get; set; }
+}
+
+[RequireAuthentication]
+[ValidateDataAnnotations]
+public class ClientWantsToDeleteBooking
+    (CourtBookingRepository bookingRepository) : BaseEventHandler<ClientWantsToDeleteBookingDto>
+{
+    public override Task Handle(ClientWantsToDeleteBookingDto dto, IWebSocketConnection socket)
+    {
+        bookingRepository.DeleteCourtBooking(dto.BookingId);
+       
+    return Task.CompletedTask;
+}
+
+}
