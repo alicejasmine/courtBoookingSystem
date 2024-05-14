6893f0e43cb79b13a513f4a0db165b8e631313ad
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-05-14 16:17:08 +0200

    tests set up with db rebuild

---
+++ tests/ApiTests.cs
@@ -0,0 +1,22 @@
+﻿using api.ClientEventHandlers;
+using api.ServerEvents;
+using NUnit.Framework;
+using Tests;
+
+namespace tests;
+
+[TestFixture]
+
+[NonParallelizable]
+public class ApiTests
+{
+ 
+    [SetUp]
+    public async Task Setup()
+    {
+        Startup.Start(null);
+        Helper.RebuildDatabase();
+    }
+
+
+}
