cefd42982409a6f4c69b5934791b1b96596cdeee
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-05-14 16:29:06 +0200

    client cannot book already booked court test

--- tests/ApiTests.cs
+++ tests/ApiTests.cs
@@ -51,5 +51,51 @@ public class ApiTests
         ws.Client.Dispose();
     
     }
+    
+    
+    [Test]
+    public async Task ClientCannotBookAlreadyBookedCourt()
+    {
+        //arrange
+        var ws1 = await new WebSocketTestClient().ConnectAsync();
+        var ws2 = await new WebSocketTestClient().ConnectAsync();
+
+        var courtBookingDto = new ClientWantsToBookCourtDto
+        {
+            CourtId = 4,
+            UserId = 1,
+            SelectedDate = new DateTime(2024, 5, 15),
+            StartTime = TimeSpan.Parse("09:00:00"), 
+            EndTime = TimeSpan.Parse("10:00:00"),
+            CreationTime = DateTime.Now
+        };
+        
+        //sign in is required for accessing booking function
+        await ws1.DoAndAssert(new ClientWantsToSignInDto() { email = "alice@gmail.com", password = "blabla" },
+            receivedMessages =>
+            {
+                return receivedMessages.Count(e => e.eventType == nameof(ServerAuthenticatesUser)) == 1;
+            });
+        
+        await ws2.DoAndAssert(new ClientWantsToSignInDto() { email = "bla@gmail.com", password = "blabla" },
+            receivedMessages =>
+            {
+                return receivedMessages.Count(e => e.eventType == nameof(ServerAuthenticatesUser)) == 1;
+            });
+
+        //act and assert
+        await ws1.DoAndAssert(courtBookingDto, receivedMessages =>
+        {
+            return receivedMessages.Count(e => e.eventType == nameof(ServerSendsConfirmationMessageToClient)) == 1;
+        });
+        await ws2.DoAndAssert(courtBookingDto, receivedMessages =>
+        {
+            return receivedMessages.Count(e => e.eventType == nameof(ServerSendsErrorMessageToClient)) == 1;
+        });
+
+        
+        ws1.Client.Dispose();
+        ws2.Client.Dispose();
+    }
 
 }
