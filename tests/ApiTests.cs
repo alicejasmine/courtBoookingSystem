a23822b1698d850f64d16225770d43ccb378b836
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-05-14 16:21:09 +0200

    add create booking test

--- tests/ApiTests.cs
+++ tests/ApiTests.cs
@@ -18,5 +18,38 @@ public class ApiTests
         Helper.RebuildDatabase();
     }
 
+    [Test]
+    public async Task ClientCanBookACourt()
+    {
+        //arrange
+        var ws = await new WebSocketTestClient().ConnectAsync();
+        
+        var courtBookingDto = new ClientWantsToBookCourtDto
+        {
+            CourtId = 4,
+            UserId = 1,
+            SelectedDate = new DateTime(2024, 5, 15),
+            StartTime = TimeSpan.Parse("09:00:00"), 
+            EndTime = TimeSpan.Parse("10:00:00"),
+            CreationTime = DateTime.Now
+        };
+        
+        //sign in is required for accessing booking function
+        await ws.DoAndAssert(new ClientWantsToSignInDto() { email = "alice@gmail.com", password = "blabla" },
+            receivedMessages =>
+            {
+                return receivedMessages.Count(e => e.eventType == nameof(ServerAuthenticatesUser)) == 1;
+            });
+
+        //act and assert
+        await ws.DoAndAssert(courtBookingDto, receivedMessages =>
+        {
+            return receivedMessages.Count(e => e.eventType == nameof(ServerSendsConfirmationMessageToClient)) == 1;
+        });
+
+        
+        ws.Client.Dispose();
+    
+    }
 
 }
